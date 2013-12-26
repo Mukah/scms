ef9ee7a6510eaeb9b0dec157721fd06c0ea3ed02
Mukah <mukah@live.com>
2013-12-26 18:17:50 -0200

    implementing classes

--- classes/class.post.php
+++ classes/class.post.php
@@ -3,29 +3,29 @@ require_once('class.database.php');
 Database::connect();
 
 class DBObject {
-	protected $obj;
-	protected $table_name;
-	protected $singular;
-
 	private $attrs = array();
 
-	function __construct($obj, $table_name, $singular) {
-		$this->obj = $obj;
-		$this->table_name = $table_name;
-		$this->singular = $singular;
+	function __construct() {
+		//$this->obj = $obj;
+		//$this->plural = $plural;
+		//$this->singular = $singular;
 	}
 
 	public function all() {
-		return "SELECT * FROM {$this->table_name};";
+		return "SELECT * FROM {$this->plural};";
 	}
-	public function find_by_id($value) {
-		$query = mysql_query("SELECT * FROM {$this->table_name} WHERE {$this->singular}_id = {$value};", Database::$connection);
-		$d = mysql_fetch_assoc($query);
+	protected static function find_by_id($obj, $value) {
+		get_class($this)
+		//$query = mysql_query("SELECT * FROM {$this->plural} WHERE {$this->singular}_id = {$value};", Database::$connection);
+		//$d = mysql_fetch_assoc($query);
 
-		foreach ($d as $key => $value) {
-			$this->obj->set($key, $value);
-		}
-		return $this->obj;
+
+
+		//$obj = new Post();
+		//foreach ($d as $key => $value) {
+		//	$obj->set($key, $value);
+		//}
+		//return $obj;
 	}
 	public function get($field) {
 		return $this->attrs[$field];
@@ -42,20 +42,29 @@ class DBObject {
 			array_push($values, "{$field} = '{$value}'");
 		}
 		$string = implode(', ', $values);
-		$query = mysql_query("UPDATE {$this->table_name} SET {$string} WHERE {$this->singular}_id = {$this->id()}");
+		return mysql_query("UPDATE {$this->plural} SET {$string} WHERE {$this->singular}_id = {$this->id()}");
 	}
 	public function id() {
 		return $this->attrs["{$this->singular}_id"];
 	}
 }
 class Post extends DBObject {
+	protected static $plural = "posts";
+	protected static $singular = "post";
+
 	function __construct() {
-		parent::__construct($this, "posts", "post");
+		parent::__construct();
+		$this->plural;
+		$this->singular;
+	}
+
+	public static function find_by_id($value) {
+
 	}
 }
 
 $post = new Post();
-$post->find_by_id(1);
+$post = $post->find_by_id(1);
 echo $post->get('title');
 $post->set('title', 'Como cuidar de um macaco simpatico');
 echo $post->get('title');
